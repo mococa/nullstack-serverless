6cc581ce4714db6d1ec2bc96df4bb466a0f18b68
Luiz Felipe Moureau <xfazxtudo@gmail.com>
2023-07-06 23:56:34 -0300

    :sparkles: feat: supported SPA and SSG build types

--- infrastructure/lib/lambda_content.js
+++ infrastructure/lib/lambda_content.js
@@ -1,11 +1,11 @@
 const serverless = require("@vendia/serverless-express");
 const { server } = require("./.production/server").default;
 
-if (!process.env.LAMBDA) {
-  server.listen(3000, () => {
+server.less = Boolean(process.env.LAMBDA);
+
+if (!process.env.LAMBDA)
+  return server.listen(3000, () => {
     console.log("Server is listening on port 3000");
   });
-} else {
-  server.less = true;
-  exports.handler = serverless({ app: server, trimStageFromRequestPath: true });
-}
+
+exports.handler = serverless({ app: server, trimStageFromRequestPath: true });
