152780f3d9cb07af52bf65318d3c13be35ccd6e4
Luiz Felipe Moureau <xfazxtudo@gmail.com>
2024-11-30 19:14:00 -0300

    🔨 refactor: fixing permissions issues on wsl

--- pulumi/aws/lib/compressNullstackApps.ts
+++ pulumi/aws/lib/compressNullstackApps.ts
@@ -1,74 +1,76 @@
 /* ---------- External ---------- */
 import { join, sep } from "path";
-import { mkdirSync, existsSync } from "fs";
+import { mkdirSync, existsSync, rmSync } from "fs";
 import AdmZip from "adm-zip";
 
 /**
  * Compresses SSR Nullstack Apps into zip files to be sent to AWS
  *
  * @param app_dirs Nullstack Apps directories
- * @returns Promise<boolean>
+ * @returns {Promise<boolean>} Whether all apps have been successfully zipped
  */
-export async function compressSSRNullstackApps(app_dirs: string[]) {
-  const build_parent = join(__dirname, "nullstack-build");
+export async function compressSSRNullstackApps(app_dirs: string[]): Promise<boolean> {
+  try {
 
-  if (!existsSync(build_parent)) mkdirSync(build_parent);
+    await Promise.all(
+      app_dirs.map(async (app_dir) => {
+        const build_parent = join(__dirname, "nullstack-build");
+        const serverless_express = join(__dirname, "..", "pkg", "@codegenie", "serverless-express");
+        const build_folder = join(build_parent, app_dir.split(sep).reverse()[0]);
+        const target = join(build_folder, "build.zip");
+        const dot_production = join(app_dir, ".production");
+        const public_folder = join(app_dir, "public");
+        const index_js_content = [
+          'const serverless = require("@codegenie/serverless-express");',
+          'const { server } = require("./.production/server.js").default;',
+          "server.less = true;",
+          "exports.handler = serverless({ app: server, trimStageFromRequestPath: true });",
+          "",
+        ].join("\n");
 
-  const serverless_express = join(
-    __dirname,
-    "..",
-    "pkg",
-    "@codegenie",
-    "serverless-express"
-  );
+        if (!existsSync(build_parent)) mkdirSync(build_parent);
+        if (!existsSync(build_folder)) mkdirSync(build_folder);
+        if (existsSync(target)) rmSync(target)
+        if (!existsSync(dot_production)) {
+          throw new Error(`Please, build this SSR nullstack app first. ${build_folder}`);
+        }
 
-  const index_js_content = [
-    'const serverless = require("@codegenie/serverless-express");',
+        // Creating zip
+        const build_zip = new AdmZip();
 
-    'const { server } = require("./.production/server.js").default;',
+        // Adding lambda entry to zip
+        build_zip.addFile("index.js", Buffer.from(index_js_content));
 
-    "server.less = true;",
+        // Adding node modules to zip
+        build_zip.addLocalFolder(
+          serverless_express,
+          join("node_modules", "@codegenie", "serverless-express")
+        );
 
-    "exports.handler = serverless({ app: server, trimStageFromRequestPath: true });",
-    "",
-  ].join("\n");
+        // Adding public folder to zip
+        build_zip.addLocalFolder(public_folder, "public");
 
-  return Promise.all(
-    app_dirs.map(async (app_dir) => {
-      const build_folder = join(build_parent, app_dir.split(sep).reverse()[0]);
+        // Adding .production folder to zip
+        if (existsSync(join(app_dir, ".production"))) {
+          build_zip.addLocalFolder(
+            dot_production,
+            ".production",
+            new RegExp("client|server"),
+          );
+        }
 
-      if (!existsSync(build_folder)) mkdirSync(build_folder);
+        // Writing zip
+        return new Promise<boolean>((resolve, reject) => {
+          build_zip.writeZip(join(build_folder, "build.zip"), (err: string) => {
+            if (err) return reject(err);
 
-      // Creating zip
-      const build_zip = new AdmZip();
-
-      const files = /(client\.(js|css))|(server\.js)/g
-
-      // Adding .production folder to zip
-      if (existsSync(join(app_dir, ".production"))) {
-        build_zip.addLocalFolder(join(app_dir, ".production"), ".production", files)
-      }
-
-      // Adding public folder to zip
-      build_zip.addLocalFolder(join(app_dir, "public"), "public");
-
-      // Adding node modules to zip
-      build_zip.addLocalFolder(
-        serverless_express,
-        join("node_modules", "@codegenie", "serverless-express")
-      );
-
-      // Adding lambda entry to zip
-      build_zip.addFile("index.js", Buffer.from(index_js_content));
-
-      // Writing zip
-      return new Promise((resolve, reject) => {
-        build_zip.writeZip(join(build_folder, "build.zip"), (err) => {
-          if (err) return reject(err);
-
-          resolve(true);
+            resolve(true);
+          });
         });
-      });
-    })
-  );
+      })
+    );
+    return true; // All succeeded
+  } catch (error) {
+    return false; // At least one failed
+  }
 }
